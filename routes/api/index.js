1483ccac8604fcf6fc99389b7ff929d70eaac420
Chris Goslin <cgoslin7@gmail.com>
2021-06-28 15:55:05 -0500

    working on calling api

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,28 @@
 const router = require("express").Router();
+document.getElementById("search").onclick = function() {myFunction()};
+var saveTodo = localStorage.getItem('todo');
+console.log(saveTodo);
+
+
+
+function myFunction() {
+    var todo = document.getElementById("card-input").value
+    var url = ``
+
+
+fetch(url)
+    .then(function(response) {
+        if (!response.ok) {
+            throw response.json();
+        }
+        return response.json()
+    }).then(function (response) {
+        console.log(response)
+
+     localStorage.setItem("todo",document.getElementById("card-input").value);
+
+    var todoEl = response.CommonName
+    $("#keyword").html(todoEl); 
+    })
+
+}
