ff6a085b1ae7680ffdfacc47e74175230dc224ed
corry cat <corrinaalcoser@gmail.com>
2021-07-03 02:06:58 -0500

    edited files

--- src/components/art_galleries/Art_Galleries.js
+++ src/components/art_galleries/Art_Galleries.js
@@ -1,33 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import TodoCard from '../card/Card';
 // import Wrapper from '../wrapper/Wrapper';
 // import todos from '../footer/footer';  this is where we will need to import different API's
 
 export default class Art_Galleries extends Component {
-    state = {
-      loading: true,
-      person:null
-    };
-    
+  state = {
+    loading: true,
+    person: null
+  };
+
   async componentDidMount() {
-    const url="https://api.randomuser.me/";
+    const url = "https://api.randomuser.me/";
     const response = await fetch(url);
     const data = await response.json();
     this.setState({ person: data.results, loading: false });
     console.log(data.results[0].name.first);
   }
 
-    render() {
-      return (
-        <div>
-          {this.state.loading || !this.state.person ? (
-            <div>loading...</div>
-          ) : (
-        <div>
-          <div>{ this.state.person[0].name.first }</div>
-          <div>{ this.state.person[0].name.last }</div>
-        </div>
-                // <Wrapper>
+  render() {
+    return (
+      <div>
+        {this.state.loading || !this.state.person ? (
+          <div>loading...</div>
+        ) : (
+          <div>
+            <div>{this.state.person[0].name.first}</div>
+            <div>{this.state.person[0].name.last}</div>
+          </div>
+          // <Wrapper>
           /* {this.state.todos.map(todo => (
             <TodoCard
               id={todo.id}
@@ -36,12 +36,12 @@ export default class Art_Galleries extends Component {
               author={todo.info}
             />
           ))} */
-        /* </Wrapper>  */
-      )}
+          /* </Wrapper>  */
+        )}
       </div>
-      );
-          }  
-        }
-  
+    );
+  }
+}
+
 
 // export default Art_Galleries;
--- src/components/modal/ModalInClassComponents.js
+++ src/components/modal/ModalInClassComponents.js
@@ -5,17 +5,18 @@ import './modal.css';
 
 
 class ModalInClassComponents extends Component {
-    state={
-        openModal : false
+    state = {
+        openModal: false
     }
 
-    onClickButton = e =>{
+    onClickButton = e => {
         e.preventDefault()
-        this.setState({openModal : true})
+        this.setState({ openModal: true })
     }
 
-    onCloseModal = ()=>{
-        this.setState({openModal : false})
+    onCloseModal = () => {
+        this.setState({ openModal: false })
+
     }
 
     render() {
@@ -26,6 +27,13 @@ class ModalInClassComponents extends Component {
                     <h1>Sign-in</h1>
                 </Modal>   
             </div>
+
+            // <div>
+            //     <button onClick={this.onClickButton}>Sign-Up</button>
+            //     <Modal open={this.state.openModals} onClose={this.onCloseModal}>
+            //         <h1>Sign-Up</h1>
+            //     </Modal>   
+            // </div>
         )
     }
 }
