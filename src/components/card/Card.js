bba4d0a7b8504b187b76e643d2b09fdf4c41947c
Samuel Cordova <cordovas1337@gmail.com>
2021-07-10 12:26:41 -0500

    added some stuff

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,22 +3,65 @@ import "./Card.css";
 
 function TodoCard(props) {
   return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.image} src={props.image} />
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong></strong> {props.title}
-          </li>
-          <li>
-            <strong></strong> {props.info}
-          </li>
-        </ul>
+    // <div className="card">
+    //   <div className="img-container">
+    //     <img alt={props.image} src={props.image} />
+    //   </div>
+    //   <div className="content">
+    //     <ul>
+    //       <li>
+    //         <strong></strong> {props.title}
+    //       </li>
+    //       <li>
+    //         <strong></strong> {props.info}
+    //       </li>
+    //     </ul>
+    //   </div>
+    // </div>
+  
+
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+        </img>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
-  
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+        </img>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+        </img>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+        </img>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
   );
 }
 
