9aa3266ce7dccc0ba5d08dfad3e5dda2b5100688
corry cat <corrinaalcoser@gmail.com>
2021-07-10 13:04:55 -0500

    added the map api js code

--- src/components/hiking/Hiking.js
+++ src/components/hiking/Hiking.js
@@ -1,29 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import TodoCard from '../card/Card';
 import Wrapper from '../wrapper/Wrapper';
 // import todos from '../footer/footer';  this is where we will need to import different API's
 
 class Hiking extends Component {
-    state= {
+  state = {
     //   todos
-    };
-  
-    render() {
-      return (
-        <Wrapper>
-          {/* {this.state.todos.map(todo => (
-            <TodoCard
-              id={todo.id}
-              title={todo.title}
-              image={todo.image}
-              author={todo.info}
-            />
-          ))} */}
+  };
 
-        <div id="hiking">this is the hiking page</div>
-        </Wrapper> 
-      );
-    }
+  render() {
+    let map;
+    return (
+
+
+      function initMap() {
+
+        const google = window.google
+
+        map = new google.maps.Map(document.getElementById("map"), {
+          scaleControl: true,
+          zoom: 4,
+          // center: { lat: +lat, lng: +lng },
+        });
+        // NOTE: This uses cross-domain XHR, and may not work on older browsers.
+        map.data.loadGeoJson(
+          " https://opendata-cosagis.opendata.arcgis.com/datasets/55234cc46b954f69a659711b240bdf59_0.geojson?outSR=%7B%22latestWkid%22%3A2278%2C%22wkid%22%3A102740%7D"
+        );
+      }
+
+      // loadMapAt(new google.maps.LatLng(geo.lat, geo.lng));
+
+
+    );
+
   }
+}
 
 export default Hiking;
